ddee41af0f80a0f1e92c2f45019d762264d3db0f
MiriamHuanca <mairim.acnauh@gmail.com>
2020-08-26 16:48:50 -0300

    creating a filter Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'creating-pipe-project';
+  servers=[
+    {
+      instanceType: 'medium',
+      name: 'Production',
+      status: 'stable',
+      started: new Date(15,1,2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15,1,2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15,1,2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15,1,2017)
+    },
+    {
+      instanceType: 'medium',
+      name: 'Production 2',
+      status: 'critical',
+      started: new Date(15,1,2017)
+    },
+  ];
+
+  filteredStatus = '';
+
+  getStatusClasses(server:{instanceType: string,name: string,status: string, started: Date}) {
+    return {
+      'list-group-item-success':server.status === 'stable',
+      'list-group-item-warning':server.status === 'offline',
+      'list-group-item-danger':server.status === 'critical',
+    }
+  }
 }
